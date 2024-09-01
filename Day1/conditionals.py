615aaa1b79e5dfddbe85b1396e132520a07d5837
kedar-pp <kedarpattanshetti@gmail.com>
2024-09-01 20:32:30 +0530

    add new file

---
+++ Day1/conditionals.py
@@ -0,0 +1,9 @@
+# convert any text in lower case
+
+day_of_week=input("Enter the Day of week: ").lower()
+print(day_of_week)
+
+# convert any text in upper case
+
+name_in_lower=input("Enter the Name in lower case: ").upper()
+print(name_in_lower)
