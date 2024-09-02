79d355a56210e2cae0cb0ad61ca44ab3f51f3b0d
kedar-pp <kedarpattanshetti@gmail.com>
2024-09-02 22:27:07 +0530

    new commit

---
+++ Day1/loops.py
@@ -0,0 +1,34 @@
+# list data structure
+# list--> data structure which can hold multipe values of multiple type
+# array--> data structure which csn hold multiple values of same type
+
+list_of_cloud=["aws","azure","gcp","digitsl ocean","utho","alibaba","oracle"]
+
+print(list_of_cloud)
+
+# add a new cloud Salesforce
+
+list_of_cloud.append("Salesforce")
+
+# add new cloud IBM cloud
+
+list_of_cloud.append("IBM cloud")
+
+print(list_of_cloud)
+
+list_of_cloud.insert(2,"Heroku")
+
+print(list_of_cloud)
+
+# for count of elements in list
+
+print(len(list_of_cloud))
+
+# iteration of list
+
+for cloud in list_of_cloud:
+    print(" ")
+    print(cloud)
+
+for i in range(1,11):
+    print(i)
