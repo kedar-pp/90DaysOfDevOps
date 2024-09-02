2f9c4eefe073c9a1e7d915d7bef36e8c5e03468c
kedar-pp <kedarpattanshetti@gmail.com>
2024-09-02 22:44:30 +0530

    new commit

--- Day1/utils.py
+++ Day1/utils.py
@@ -2,3 +2,11 @@
 #                     |--> OS (operating sys library)
 #                     |--> shutil (shell utlis)
 #                     |--> system (sys level)
+
+
+# Importing a library for os
+import os 
+
+# linuc cmd to see diskspace i.e df -h
+print(os.system('df -h'))
+
