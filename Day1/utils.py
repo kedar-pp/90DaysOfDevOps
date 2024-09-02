a9f74c62e62fb06b639041b8106b94c3d0aa31f6
kedar-pp <kedarpattanshetti@gmail.com>
2024-09-02 22:51:57 +0530

    new commit

--- Day1/utils.py
+++ Day1/utils.py
@@ -10,6 +10,11 @@ import os
 # linuc cmd to see diskspace i.e df -h
 print(os.system('df -h'))
 
-# we can see kab se sys chalu hey
+# we can see kab se sys chalu hey i.e uptime and load average
 
-print(os.system('uptime'))
+print(os.system('uptime')) #for linux and mac
+
+
+print(os.system('systeminfo')) #for windows
+
+print(os.system('sysctl hw.memsize')) #for linux and mac
