6f8aa72ab91c0d7ae3cf54d3578dd0dd130e49c7
kedar-pp <kedarpattanshetti@gmail.com>
2024-09-04 22:26:49 +0530

    new commit

---
+++ mini_project/s3_backup.py
@@ -0,0 +1,8 @@
+"""
+This is a script to take a backup from local to AWS S3
+boto3-->used to do AWS task using python
+pip(python package installer) cmd for installing boto3
+pip install boto3
+
+"""
+import boto3
